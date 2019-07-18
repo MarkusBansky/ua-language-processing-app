2498e3806255a560ac71e65eed28f0456c7f9a29
Markiian Benovskyi <benovskym@gmail.com>
2019-07-18 16:11:23 +0100

    Update Utils.ts

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,21 +1,14 @@
+import _ from 'lodash'
 import VariationTag from "../processors/parts/VariationTag"
 import SentenceWord from "../processors/parts/SentenceWord"
-import Sentence from '../processors/parts/Sentence';
-import { WordTagColors } from './Colors';
+import Sentence from '../processors/parts/Sentence'
+import { WordTagColors } from './Colors'
 
-export function sortCollectionByParam(field, reverse, primer){
-
-  var key = primer ?
-      function(x) {return primer(x[field])} :
-      function(x) {return x[field]};
-
-  reverse = !reverse ? 1 : -1;
-
-  return function (a, b) {
-    return a = key(a), b = key(b), reverse * (((a > b) ? 1 : -1) - ((b > a) ? 1 : -1));
-  }
-}
-
+/**
+ * Searches for a desired word in chosen array of sentences.
+ * @param wordId String of ID by which to search for a word.
+ * @param sentences Array of sentences where to search.
+ */
 export function findWordInSentences
   (wordId: string, sentences: Sentence[]): SentenceWord | undefined {
   for (var i = 0; i < sentences.length; i++) {
@@ -26,47 +19,62 @@ export function findWordInSentences
   return undefined
 }
 
+/**
+ * Generates color string for the selected word [posTag].
+ * @param word An object of type [[SentenceWord]] that color is generated for.
+ */
 export function generateColorForWord(word: SentenceWord): string {
   const { wordValue } = word;
   const variation = word.getBestVariation()
 
+  // If there is no word or it does not have a variation or
+  // the variation does not have a tag then return nothing
   if (!wordValue || !variation || !variation.posTag) return '';
 
-  // TODO: Here is a color for the word that has no best variation and has multiple tag variations
-  let sortedVariations = word.variations.sort((a, b) => a.probability > b.probability ? 1 : -1)
-  if (sortedVariations.length > 1 && sortedVariations[0].probability - sortedVariations[1].probability <= 0.5 && sortedVariations.some(v => v.additionalTags.some(t => t.name.startsWith('v_')))) {
+  // Display a color for the word that has no best variation and
+  // has multiple tag variations
+  // Sort the variations
+  let sortedVariations = _.sortBy(word.variations, v => v.probability)
+  // Check the conditions if rthere are any two variations
+  if (sortedVariations.length > 1
+    // And the probability is far from distinct
+    && sortedVariations[0].probability - sortedVariations[1].probability <= 0.5
+    // And there are some tags with v_ that FIXME:Later
+    && sortedVariations.some(v => v.additionalTags.some(t => t.name.startsWith('v_')))) {
+    // Return UNPREDICTED
     return WordTagColors.Unpredicted
   }
 
+  // Otherwise for each pos tag name return it's color
   if (variation.posTag.name === 'noun') return WordTagColors.Noun
   if (variation.posTag.name === 'adj') return WordTagColors.Adjective
   if (variation.posTag.name === 'adv') return WordTagColors.Adverb
   if (variation.posTag.name === 'verb') return WordTagColors.Verb
 
+  // If there is no color for a tag then return nothing
   return ''
 }
 
+/**
+ * Sets first letter to upper case and all others to lower case.
+ * @param s The text that has to be modified.
+ */
 export function toFirstUpperLetter(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-
-export function extractPosTag(tags: any): VariationTag | null {
+/**
+ * Looks for the main tag in the array of tags from API. Returns a [VariationTag].
+ * @param tags An array of tag objects returned from API
+ */
+export function extractPosTag(tags: any[]): VariationTag | null {
+  // Loop through every tag
   for (var i = 0; i < tags.length; i++)
+    // If this tag has a flag of main part of speech
     if (tags[i].isMainPartOfSpeech)
+      // Then return the result
       return new VariationTag(tags[i])
 
-  if (tags.length === 0) {
-    return tags[0]
-  }
+  // Otherwise if the length of tags is 0 return null
   return null
-}
-
-export function generateTrainWord(
-  word: SentenceWord, tagLeftId: number, tagRightId: number) {
-  return {
-    tagId: word.getBestVariation()!.posTag!.id,
-    tagLeftId,
-    tagRightId
-  }
 }
